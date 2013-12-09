70eb6f15cdb2de266e66cb6afa688e900b17d87c
rubzyk <baillepierre@gmail.com>
2013-12-09 20:45:38 +0100

    comment base classes

--- src/bartok/mode.clj
+++ src/bartok/mode.clj
@@ -1,50 +1,50 @@
-(ns bartok.mode
-  (:use [bartok.constants])
-  (:use [bartok.litterals.identity])
-  (:use [bartok.pitch-class])
-  (:use [bartok.pitch])
-  (:use [bartok.m-degree])
-  (:use [utils.utils]))
+; (ns bartok.mode
+;   (:use [bartok.constants])
+;   (:use [bartok.litterals.identity])
+;   (:use [bartok.pitch-class])
+;   (:use [bartok.pitch])
+;   (:use [bartok.m-degree])
+;   (:use [utils.utils]))
 
-(defn mode? [m] (= (set (keys m)) #{:name :mother :degree :degrees :prio}))
+; (defn mode? [m] (= (set (keys m)) #{:name :mother :degree :degrees :prio}))
 
 
-(def modes
-  (set (map
-    (fn [[ch-name moth-name]]
-      (let [moth-hash (mother-modes moth-name)
-            degree (->> moth-hash :childs (pos #{ch-name}) first inc)
-            prio   (-> moth-hash :modes_prio (nth (dec degree)))
-            degrees (vec (sort #(< (m-degree-dist %1) (m-degree-dist %2)) prio))]
-        {:name ch-name :mother moth-name :degree degree :prio prio :degrees degrees}))
-    mother)))
+; (def modes
+;   (set (map
+;     (fn [[ch-name moth-name]]
+;       (let [moth-hash (mother-modes moth-name)
+;             degree (->> moth-hash :childs (pos #{ch-name}) first inc)
+;             prio   (-> moth-hash :modes_prio (nth (dec degree)))
+;             degrees (vec (sort #(< (m-degree-dist %1) (m-degree-dist %2)) prio))]
+;         {:name ch-name :mother moth-name :degree degree :prio prio :degrees degrees}))
+;     mother)))
 
-(defn abs-mode-name? [k] (k mother))
+; (defn abs-mode-name? [k] (k mother))
 
-(defn split-mode-name [mn]
-  (let [[r m] (map keyword (clojure.string/split (name mn) #"\-"))]
-    (if (and (abs-mode-name? m) (pitch-class-kw? r)) [r m] nil)))
+; (defn split-mode-name [mn]
+;   (let [[r m] (map keyword (clojure.string/split (name mn) #"\-"))]
+;     (if (and (abs-mode-name? m) (pitch-class-kw? r)) [r m] nil)))
 
-(defn mode-name? [k]
-  (if (split-mode-name k) true false))
+; (defn mode-name? [k]
+;   (if (split-mode-name k) true false))
 
-(defn abs-mode [& args]
-  (let [[a b c d] args]
-    (cond
-      (abs-mode-name? a)
-        (select-first #(= a (:name %)) modes)
-      (map? a)
-        (select-first #(submap? a %) modes))))
+; (defn abs-mode [& args]
+;   (let [[a b c d] args]
+;     (cond
+;       (abs-mode-name? a)
+;         (select-first #(= a (:name %)) modes)
+;       (map? a)
+;         (select-first #(submap? a %) modes))))
 
-(defmulti mode
-  (fn [& args] (let [[a b c d] args]
-     (cond
-      (mode-name? a) :mode-name))))
+; (defmulti mode
+;   (fn [& args] (let [[a b c d] args]
+;      (cond
+;       (mode-name? a) :mode-name))))
 
-(defmethod mode :mode-name [n]
-  (apply #(into (abs-mode %2)
-                (hash-map :root %1))
-    (split-mode-name n)))
+; (defmethod mode :mode-name [n]
+;   (apply #(into (abs-mode %2)
+;                 (hash-map :root %1))
+;     (split-mode-name n)))
 
 
 
--- src/bartok/pitch_class.clj
+++ src/bartok/pitch_class.clj
@@ -1,55 +1,55 @@
-(ns bartok.pitch-class
-  (:use [bartok.constants])
-  (:use [utils.utils]))
+; (ns bartok.pitch-class
+;   (:use [bartok.constants])
+;   (:use [utils.utils]))
 
-(defprotocol IPitchClass
-  (unaltered-name [this])
-  (alt-name [this])
-  (alt-val [this]))
+; (defprotocol IPitchClass
+;   (unaltered-name [this])
+;   (alt-name [this])
+;   (alt-val [this]))
 
-(defrecord PitchClass [name c-dist]
-  IPitchClass
-  (unaltered-name [this] (subs name 0 1))
-  (alt-name [this] (subs name 1))
-  (alt-val [this] (alterations-values (alt-name this))))
+; (defrecord PitchClass [name c-dist]
+;   IPitchClass
+;   (unaltered-name [this] (subs name 0 1))
+;   (alt-name [this] (subs name 1))
+;   (alt-val [this] (alterations-values (alt-name this))))
 
-;********** Constructor **************
+; ;********** Constructor **************
 
-(defmulti pitch-class
-  (fn [& args]
-    (cond
-      (count= args 1)
-        (if (or (keyword? (first args)) (string? (first args))) 
-            :name
-            :integer)
-      (count= args 2) 
-        (if (and (or (keyword? (first args)) (string? (first args)))
-                 (number? (second args)))
-            [:unaltered-name :c-dist]))))
+; (defmulti pitch-class
+;   (fn [& args]
+;     (cond
+;       (count= args 1)
+;         (if (or (keyword? (first args)) (string? (first args))) 
+;             :name
+;             :integer)
+;       (count= args 2) 
+;         (if (and (or (keyword? (first args)) (string? (first args)))
+;                  (number? (second args)))
+;             [:unaltered-name :c-dist]))))
 
 
-(defmethod pitch-class :name [n]
-  (let [name-str (name n)
-        unaltered-name (subs name-str 0 1)
-        alt-name (subs name-str 1)
-        alt-val  (or (alterations-values alt-name) 0)
-        c-dist (+ (unaltered-pitch-classes (keyword unaltered-name)) alt-val)]
-    (->PitchClass name-str c-dist)))
+; (defmethod pitch-class :name [n]
+;   (let [name-str (name n)
+;         unaltered-name (subs name-str 0 1)
+;         alt-name (subs name-str 1)
+;         alt-val  (or (alterations-values alt-name) 0)
+;         c-dist (+ (unaltered-pitch-classes (keyword unaltered-name)) alt-val)]
+;     (->PitchClass name-str c-dist)))
 
-(defmethod pitch-class :integer [i] 
-  (let [n (pitch-class-defaults-names i)] (pitch-class n)))
+; (defmethod pitch-class :integer [i] 
+;   (let [n (pitch-class-defaults-names i)] (pitch-class n)))
 
-(defmethod pitch-class [:unaltered-name :c-dist]
-  [un cd]
-  (let [diff (- cd (unaltered-pitch-classes (keyword un)))
-        alt-val (cond (< 2 diff) (- diff 12) (> -2 diff) (+ diff 12) :else diff)
-        alt-name (alterations-names alt-val)
-        name-str (str (name un) alt-name)]
-    (->PitchClass name-str cd)))
+; (defmethod pitch-class [:unaltered-name :c-dist]
+;   [un cd]
+;   (let [diff (- cd (unaltered-pitch-classes (keyword un)))
+;         alt-val (cond (< 2 diff) (- diff 12) (> -2 diff) (+ diff 12) :else diff)
+;         alt-name (alterations-names alt-val)
+;         name-str (str (name un) alt-name)]
+;     (->PitchClass name-str cd)))
 
-;*************** Methods *****************
+; ;*************** Methods *****************
 
-(defn pitch-class? [this] (= (class this) bartok.pitch_class.PitchClass))
+; (defn pitch-class? [this] (= (class this) bartok.pitch_class.PitchClass))
 
 
 
