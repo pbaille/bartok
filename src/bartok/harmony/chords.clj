c622dbc5a00be6b40e1b5bfea10816712d375934
rubzyk <baillepierre@gmail.com>
2014-02-19 15:50:22 +0100

    predicate constructors

--- src/bartok/harmony/chords.clj
+++ src/bartok/harmony/chords.clj
@@ -20,7 +20,7 @@
 (def ^:private b9s 
   (range 13 127 12)); => (13 25 37 49 61 73 85 97 109 121)
 
-;;; drop preds ;;;
+;;;;;;;;;;;;;; drop preds ;;;;;;;;;;;;;;;;;;;;
 
 (defn b9-free? 
   "return true if chord has no b9s (see b9s def above) in its inner voices
@@ -46,7 +46,64 @@
   (fn [drop] 
     (= (- (last drop) (first drop)) 
        siz)))
-;;;;;;;;;;;;;;;;;;
+
+(defn make-drop-pred 
+  "take a function on a drop 
+  return a function that check if (comparator (fun drop) value) for all comp-val-duplets
+  ex: (let [grav-check (make-drop-pred grav-center > 0.5 < 0.8)] 
+        (grav-check [0 15 20]) ;=> true 
+        (grav-check [0 5 20])  ;=> false)"
+  [fun & [comparator value & comp-val-duplets]]
+  (fn [drop] 
+      (and 
+       (comparator (fun drop) value)
+       (if (second comp-val-duplets)
+         ((ap make-drop-pred fun comp-val-duplets) drop)
+         true))))
+
+;;;;;;;;;;; drop analysis ;;;;;;;;;;;;;;;;;
+
+(defn median 
+  ([drop] 
+    (/ (reduce + drop) (count drop)))
+  ;pred constructor for use in drop validator
+  ([compar value & comp-val-duplets]
+    (ap make-drop-pred median compar value comp-val-duplets)))
+
+(defn grav-center 
+  "ex: (grav-center [0 3 10]) => 0.3"
+  ([[fi & nxt :as drop]]
+    (let [cnt (count drop)
+          amplitude (- (last drop) fi)
+          from-zero (map #(- % fi) drop)
+          inner-sum (reduce + (next (butlast from-zero)))]
+      (float 
+        (/ (/ inner-sum (- cnt 2))
+           amplitude))))
+  ;pred constructor for use in drop validator
+  ([compar value & comp-val-duplets]
+    (ap make-drop-pred grav-center compar value comp-val-duplets)))
+
+(defn mean-interval 
+  "ex: (mean-interval [1 2 3 4]) => 1"
+  ([[fi :as drop]]
+    (/ (- (last drop) fi) (dec (count drop))))
+  ;pred constructor for use in drop validator
+  ([compar value & comp-val-duplets]
+    (ap make-drop-pred mean-interval compar value comp-val-duplets)))
+
+(defn uniformity 
+  "repartition uniformity, 0 is perfect uniformity
+   ex : (uniformity [1 2 3 4 5]) => 0 "
+  ([drop]
+    (let [mean-inter (mean-interval drop)
+          mean-dists (map #(abs (- mean-inter %)) (steps drop))]
+      (median mean-dists)))
+  ;pred constructor for use in drop validator
+  ([compar value & comp-val-duplets]
+    (ap make-drop-pred uniformity compar value comp-val-duplets)))
+
+;;;;;;;;;;;;; Validator ;;;;;;;;;;;;;;;
 
 (defn- drop-validator 
   "return a validator function for drops"
@@ -57,6 +114,8 @@
 (def ^:private default-validator 
   (drop-validator b9-free? no-m2-on-top?))
 
+;;;;;;;;;;;; options ;;;;;;;;;;;;;;;;;
+
 (def ^:private default-options
   {:max-step 9 
    :max-size 36 
@@ -65,6 +124,58 @@
 
 (defn- merge-with-default-options [opt]
   (if-nil-merge opt default-options))
+
+;;;;;;;;;; occ-map related ;;;;;;;;;;;
+
+(defn- occ-map->seq 
+  "(occ-map->seq {:M2 2 :m3 1 :M6 2 :M7 1}) 
+   => (2 3 9 11 14 21)"
+  [om] 
+  (sort 
+    (mapcat 
+      (fn [[k v]]
+        (for [i (range v)] 
+          (+ (* i 12) (:val (b> k))))) 
+      om)))
+
+(defn- occ-repartition 
+  "return a occ repartition vector
+   ex: (occ-repartition 5 11) => [3 2 2 2 2]"
+  [size n]
+  (loop [acc (fill-with [] size 0) i 0] 
+    (if (< i n) 
+      (recur (update-in acc [(mod i size)] inc) (inc i)) 
+      acc)))
+
+(b-multi build-occ-map 
+  (['ModeClass :number][mode-class n-voices]
+    (let [main-degs (mapv :name (:main-degrees (w-mode-class mode-class)))
+          size (inc (count main-degs)) ;inc it because :P1 is implicit
+          occs (occ-repartition size n-voices)]
+      (zipmap (conj main-degs :P1) occs)))
+  ([['CIntervalClass] :number][degrees n-voices]
+    (let [size (inc (count degrees)) ;inc it because :P1 is implicit
+          occs (occ-repartition size n-voices)]
+      (zipmap (conj (mapv :name degrees) :P1) occs))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn- chord-inversions 
+  "takes a drop and returns its inversions
+  ex:
+  (chord-inversions [0 2 3 9 14])
+  => [[0 2 3 9 14] [2 3 9 12 14] [3 9 12 14 26] [9 12 14 15 26]]"
+  [d]
+  (iterate-while
+    #(<= (second (last %)) 12) 
+    (fn [acc]
+      (let [[fl & others :as l] (last acc)
+            oc (loop [x (+ fl 12)] 
+                 (if (in? others x) (recur (+ 12 x)) x))]
+        (conj acc (vec (sort (conj others oc))))))
+    [d]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn- drops 
   "return a seq of all possible drops of a chord
@@ -105,35 +216,7 @@
         ;drop is not allowed so recur with (inc idx)
         (drops coll (inc idx) max-step max-size validator))))))
 
-(defn- occ-map->seq 
-  "(occ-map->seq {:M2 2 :m3 1 :M6 2 :M7 1}) 
-   => (2 3 9 11 14 21)"
-  [om] 
-  (sort 
-    (mapcat 
-      (fn [[k v]]
-        (for [i (range v)] 
-          (+ (* i 12) (:val (b> k))))) 
-      om)))
-
-(defn- chord-inversions 
-  "takes a drop and returns its inversions
-  ex:
-  (chord-inversions [0 2 3 9 14])
-  => [[0 2 3 9 14] [2 3 9 12 14] [3 9 12 14 26] [9 12 14 15 26]]"
-  [d]
-  (iterate-while
-    #(<= (second (last %)) 12) 
-    (fn [acc]
-      (let [[fl & others :as l] (last acc)
-            oc (loop [x (+ fl 12)] 
-                 (if (in? others x) (recur (+ 12 x)) x))]
-        (conj acc (vec (sort (conj others oc))))))
-    [d]))
-
 ;;;;;;;;;;;;;;; public ;;;;;;;;;;;;;;;;
-
-
 
 (defn all-drops 
   "compute all possible drops of a chord
@@ -154,26 +237,6 @@
         (let [invs (chord-inversions (occ-map->seq occ-map))]
           (map #(drops % max-step (+ max-size (first %)) validator) invs))
         (drops (occ-map->seq occ-map) max-step max-size validator)))))
-
-(defn- occ-repartition 
-  "return a occ repartition vector
-   ex: (occ-repartition 5 11) => [3 2 2 2 2]"
-  [size n]
-  (loop [acc (fill-with [] size 0) i 0] 
-    (if (< i n) 
-      (recur (update-in acc [(mod i size)] inc) (inc i)) 
-      acc)))
-
-(b-multi build-occ-map 
-  (['ModeClass :number][mode-class n-voices]
-    (let [main-degs (mapv :name (:main-degrees (w-mode-class mode-class)))
-          size (inc (count main-degs)) ;inc it because :P1 is implicit
-          occs (occ-repartition size n-voices)]
-      (zipmap (conj main-degs :P1) occs)))
-  ([['CIntervalClass] :number][degrees n-voices]
-    (let [size (inc (count degrees)) ;inc it because :P1 is implicit
-          occs (occ-repartition size n-voices)]
-      (zipmap (conj (mapv :name degrees) :P1) occs))))
 
 (b-multi voicings
   "return a list of [Pitch] voicings accordingly with args..."
@@ -276,32 +339,7 @@
          shuffle 
          first)))
 
-(defn median [drop] 
-  (/ (reduce + drop) (count drop)))
 
-(defn grav-center 
-  "ex: (grav-center [0 3 10]) => 0.3"
-  [[fi & nxt :as drop]]
-  (let [cnt (count drop)
-        amplitude (- (last drop) fi)
-        from-zero (map #(- % fi) drop)
-        inner-sum (reduce + (next (butlast from-zero)))]
-    (float 
-      (/ (/ inner-sum (- cnt 2))
-         amplitude))))
-
-(defn mean-interval 
-  "ex: (mean-interval [1 2 3 4]) => 1"
-  [[fi :as drop]]
-  (/ (- (last drop) fi) (dec (count drop))))
-
-(defn uniformity 
-  "repartition uniformity, 0 is perfect uniformity
-   ex : (uniformity [1 2 3 4 5]) => 0 "
-  [drop]
-  (let [mean-inter (mean-interval drop)
-        mean-dists (map #(abs (- mean-inter %)) (steps drop))]
-    (median mean-dists)))
 
 (uniformity [1 2 3 4 50])
 
