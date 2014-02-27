78f97a222474183addac8b29ff11016e9cf39324
rubzyk <baillepierre@gmail.com>
2014-02-27 12:09:22 +0100

    add lower-interval-limit to voicings multi

--- src/bartok/harmony/chords.clj
+++ src/bartok/harmony/chords.clj
@@ -5,175 +5,196 @@
   (:use bartok.types.w-mode)
   (:use [utils.all :exclude [median]]))
 
-(def ^:private lower-interval-limits
-  {:m2 :E-1
-   :M2 :Eb-1
-   :m3 :C-1
-   :M3 :Bb-2
-   :P4 :Bb-2
-   :+4 :Bb-2
-   :P5 :Bb-3
-   :+5 :G-2
-   :M6 :F-2
-   :m7 :F-2
-   :M7 :F-2})
+;;;;;;;;;;;;; constants ;;;;;;;;;;;;;;
 
-(def ^:private b9s 
-  (range 13 127 12)); => (13 25 37 49 61 73 85 97 109 121)
+  (def ^:private lower-interval-limits
+    "assoc each interval to his corresponding lower-limit"
+    {:P1 :C-5
+     :m2 :F-1
+     :M2 :F-1
+     :m3 :Eb-1
+     :M3 :D-1
+     :P4 :Eb-1
+     :+4 :E-1
+     :P5 :F-2
+     :+5 :Eb-1
+     :M6 :D-1
+     :m7 :Eb-1
+     :M7 :E-1})
 
-;;;;;;;;;;;;;; drop preds ;;;;;;;;;;;;;;;;;;;;
+  (def ^:private int-lil 
+    "integer version of lower-interval-limits"
+    (map-h* (c :val b>) lower-interval-limits))
 
-(defn b9-free? 
-  "return true if chord has no b9s (see b9s def above) in its inner voices
-  means that b9 like intervals are allowed from Bass"
-  [[bass frst & nxt :as chord]]
-  (if-not (seq nxt)
-    true
-    (let [[x :as dists] (map #(- % frst) nxt)
-          res (if (= 1 x) 
-                true ; if first dist = 1 b9 is allowed (bill evans trick) 
-                ;if-not check if dists contains no b9 
-                (not (some (set dists) b9s)))] 
-      (and res (b9-free? (next chord))))))
+  (def ^:private b9s 
+    (range 13 127 12)); => (13 25 37 49 61 73 85 97 109 121)
 
-(defn no-m2-on-top? 
-  "check if a drop havn't a m2 between two top notes"
-  [d] 
-  (not= 1 (last (steps d))))
+;;;;;;;;;;;; drop preds ;;;;;;;;;;;;;;
 
-(defn size=? 
-  "return a function that check if drop is of size siz"
-  [siz]
-  (fn [drop] 
-    (= (- (last drop) (first drop)) 
-       siz)))
+  (defn lower-limits-ok? 
+    "check if a drop doesn't exceed lower-interval-limits with given root-val"
+    [root-val drop]
+    (->> (map (juxt (f> c-interval-class :val) (p + root-val)) drop)
+         (every? (fn [[x y]] (<= (int-lil x) y)))))
 
-(defn make-drop-pred 
-  "take a function 'fun' on a drop 
-  return a function that check if (comparator (fun drop) value) for all comp-val-duplets
-  ex: (let [grav-check (make-drop-pred grav-center > 0.5 < 0.8)] 
-        (grav-check [0 15 20]) ;=> true 
-        (grav-check [0 5 20])  ;=> false)"
-  [fun & [comparator value & comp-val-duplets]]
+  (defn b9-free? 
+    "return true if chord has no b9s (see b9s def above) in its inner voices
+    means that b9 like intervals are allowed from Bass"
+    [[bass frst & nxt :as chord]]
+    (if-not (seq nxt)
+      true
+      (let [[x :as dists] (map #(- % frst) nxt)
+            res (if (= 1 x) 
+                  true ; if first dist = 1 b9 is allowed (bill evans trick) 
+                  ;if-not check if dists contains no b9 
+                  (not (some (set dists) b9s)))] 
+        (and res (b9-free? (next chord))))))
+
+  (defn no-m2-on-top? 
+    "check if a drop havn't a m2 between two top notes"
+    [d] 
+    (not= 1 (last (steps d))))
+
+  (defn size=? 
+    "return a function that check if drop is of size siz"
+    [siz]
     (fn [drop] 
-      (and 
-        (comparator (fun drop) value)
-        (if (second comp-val-duplets)
-          ((ap make-drop-pred fun comp-val-duplets) drop)
-          true))))
-
-;; buggy, try to make a macro for avoiding multiple evaluation of (fun drop) ...
-; (defmacro make-drop-pred 
-;   [fun & comp-val-duplets]
-;   `(fn [x#] 
-;      (let [res# (~fun x#)] 
-;        (and ~@(map (fn [[c v]] (list c res# v)) (partition 2 comp-val-duplets))))))
-
-;;;;;;;;;;; drop analysis ;;;;;;;;;;;;;;;;;
-
-(defn median 
-  ([drop] 
-    (/ (reduce + drop) (count drop)))
-  ;pred constructor for use in drop validator
-  ([compar value & comp-val-duplets]
-    (ap make-drop-pred median compar value comp-val-duplets)))
-
-(defn grav-center 
-  "ex: (grav-center [0 3 10]) => 0.3"
-  ([[fi & nxt :as drop]]
-    (let [cnt (count drop)
-          amplitude (- (last drop) fi)
-          from-zero (map #(- % fi) drop)
-          inner-sum (reduce + (next (butlast from-zero)))]
-      (float 
-        (/ (/ inner-sum (- cnt 2))
-           amplitude))))
-  ;pred constructor for use in drop validator
-  ([compar value & comp-val-duplets]
-    (ap make-drop-pred grav-center compar value comp-val-duplets)))
-
-(defn mean-interval 
-  "ex: (mean-interval [1 2 3 4]) => 1"
-  ([[fi :as drop]]
-    (/ (- (last drop) fi) (dec (count drop))))
-  ;pred constructor for use in drop validator
-  ([compar value & comp-val-duplets]
-    (ap make-drop-pred mean-interval compar value comp-val-duplets)))
-
-(defn uniformity 
-  "repartition uniformity, 0 is perfect uniformity
-   ex : (uniformity [1 2 3 4 5]) => 0 "
-  ([drop]
-    (let [mean-inter (mean-interval drop)
-          mean-dists (map #(abs (- mean-inter %)) (steps drop))]
-      (median mean-dists)))
-  ;pred constructor for use in drop validator
-  ([compar value & comp-val-duplets]
-    (ap make-drop-pred uniformity compar value comp-val-duplets)))
-
-;;;;;;;;;;;;; Validator ;;;;;;;;;;;;;;;
-
-(defn drop-validator 
-  "return a validator function for drops"
-  [& preds]
-  (fn [drop]
-    (a satisfies-all? drop preds)))
-
-(def ^:private default-validator 
-  (drop-validator b9-free? no-m2-on-top?))
-
-(defn default-validator-and
-  "assoc given preds to the default validator defined above"
-  [& preds]
-  (ap drop-validator default-validator preds))
+      (= (- (last drop) (first drop)) 
+         siz)))
 
 
+  (defn make-drop-pred 
+    "take a function 'fun' on a drop 
+    return a function that check if (comparator (fun drop) value) for all comp-val-duplets
+    ex: (let [grav-check (make-drop-pred grav-center > 0.5 < 0.8)] 
+          (grav-check [0 15 20]) ;=> true 
+          (grav-check [0 5 20])  ;=> false)"
+    [fun & [comparator value & comp-val-duplets]]
+      (fn [drop] 
+        (and 
+          (comparator (fun drop) value)
+          (if (second comp-val-duplets)
+            ((ap make-drop-pred fun comp-val-duplets) drop)
+            true))))
 
-;;;;;;;;;;;; options ;;;;;;;;;;;;;;;;;
+  ;; buggy, try to make a macro for avoiding multiple evaluation of (fun drop) ...
+  ; (defmacro make-drop-pred 
+  ;   [fun & comp-val-duplets]
+  ;   `(fn [x#] 
+  ;      (let [res# (~fun x#)] 
+  ;        (and ~@(map (fn [[c v]] (list c res# v)) (partition 2 comp-val-duplets))))))
 
-(def ^:private default-options
-  {:max-step 9 
-   :max-size 36 
-   :inversions false 
-   :validator default-validator})
+;;;;;;;;;;; drop analysis ;;;;;;;;;;;;
 
-(defn- merge-with-default-options [opt]
-  (if-nil-merge opt default-options))
+  (defn amplitude
+    ([drop] 
+      (- (last drop)(first drop)))
+    ;pred constructor for use in drop validator
+    ([compar value & comp-val-duplets]
+      (ap make-drop-pred amplitude compar value comp-val-duplets)))
+
+  (defn median 
+    ([drop] 
+      (/ (reduce + drop) (count drop)))
+    ;pred constructor for use in drop validator
+    ([compar value & comp-val-duplets]
+      (ap make-drop-pred median compar value comp-val-duplets)))
+
+  (defn grav-center 
+    "ex: (grav-center [0 3 10]) => 0.3"
+    ([[fi & nxt :as drop]]
+      (let [cnt (count drop)
+            amplitude (- (last drop) fi)
+            from-zero (map #(- % fi) drop)
+            inner-sum (reduce + (next (butlast from-zero)))]
+        (float 
+          (/ (/ inner-sum (- cnt 2))
+             amplitude))))
+    ;pred constructor for use in drop validator
+    ([compar value & comp-val-duplets]
+      (ap make-drop-pred grav-center compar value comp-val-duplets)))
+
+  (defn mean-interval 
+    "ex: (mean-interval [1 2 3 4]) => 1"
+    ([[fi :as drop]]
+      (/ (- (last drop) fi) (dec (count drop))))
+    ;pred constructor for use in drop validator
+    ([compar value & comp-val-duplets]
+      (ap make-drop-pred mean-interval compar value comp-val-duplets)))
+
+  (defn uniformity 
+    "repartition uniformity, 0 is perfect uniformity
+     ex : (uniformity [1 2 3 4 5]) => 0 "
+    ([drop]
+      (let [mean-inter (mean-interval drop)
+            mean-dists (map #(abs (- mean-inter %)) (steps drop))]
+        (median mean-dists)))
+    ;pred constructor for use in drop validator
+    ([compar value & comp-val-duplets]
+      (ap make-drop-pred uniformity compar value comp-val-duplets)))
+
+;;;;;;;;;;;;; Validator ;;;;;;;;;;;;;;
+
+  (defn drop-validator 
+    "return a validator function for drops"
+    [& preds]
+    (fn [drop]
+      (a satisfies-all? drop preds)))
+  
+  (defn add-validator [validator & preds]
+    (ap drop-validator validator preds))
+  
+  (def ^:private default-validator 
+    (drop-validator b9-free? no-m2-on-top?))
+
+  (defn default-validator-and
+    "assoc given preds to the default validator defined above"
+    [& preds]
+    (ap drop-validator default-validator preds))
+
+;;;;;;;;;;;;; options ;;;;;;;;;;;;;;;;
+
+  (def ^:private default-options
+    {:max-step 9 
+     :max-size 36 
+     :inversions false 
+     :validator default-validator})
+
+  (defn- merge-with-default-options [opt]
+    (if-nil-merge opt default-options))
 
 ;;;;;;;;;; occ-map related ;;;;;;;;;;;
 
-(defn- occ-map->seq 
-  "(occ-map->seq {:M2 2 :m3 1 :M6 2 :M7 1}) 
-   => (2 3 9 11 14 21)"
-  [om] 
-  (sort 
-    (mapcat 
-      (fn [[k v]]
-        (for [i (range v)] 
-          (+ (* i 12) (:val (b> k))))) 
-      om)))
+  (defn- occ-map->seq 
+    "(occ-map->seq {:M2 2 :m3 1 :M6 2 :M7 1}) 
+     => (2 3 9 11 14 21)"
+    [om] 
+    (sort 
+      (mapcat 
+        (fn [[k v]]
+          (for [i (range v)] 
+            (+ (* i 12) (:val (b> k))))) 
+        om)))
 
-(defn- occ-repartition 
-  "return a occ repartition vector
-   ex: (occ-repartition 5 11) => [3 2 2 2 2]"
-  [size n]
-  (loop [acc (fill-with [] size 0) i 0] 
-    (if (< i n) 
-      (recur (update-in acc [(mod i size)] inc) (inc i)) 
-      acc)))
+  (defn- occ-repartition 
+    "return a occ repartition vector
+     ex: (occ-repartition 5 11) => [3 2 2 2 2]"
+    [size n]
+    (loop [acc (fill-with [] size 0) i 0] 
+      (if (< i n) 
+        (recur (update-in acc [(mod i size)] inc) (inc i)) 
+        acc)))
 
-(b-multi build-occ-map 
-  (['ModeClass :number][mode-class n-voices]
-    (let [main-degs (mapv :name (:main-degrees (w-mode-class mode-class)))
-          size (inc (count main-degs)) ;inc it because :P1 is implicit
-          occs (occ-repartition size n-voices)]
-      (zipmap (conj main-degs :P1) occs)))
-  ([['CIntervalClass] :number][degrees n-voices]
-    (let [size (inc (count degrees)) ;inc it because :P1 is implicit
-          occs (occ-repartition size n-voices)]
-      (zipmap (conj (mapv :name degrees) :P1) occs))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (b-multi build-occ-map 
+    (['ModeClass :number][mode-class n-voices]
+      (let [main-degs (mapv :name (:main-degrees (w-mode-class mode-class)))
+            size (inc (count main-degs)) ;inc it because :P1 is implicit
+            occs (occ-repartition size n-voices)]
+        (zipmap (conj main-degs :P1) occs)))
+    ([['CIntervalClass] :number][degrees n-voices]
+      (let [size (inc (count degrees)) ;inc it because :P1 is implicit
+            occs (occ-repartition size n-voices)]
+        (zipmap (conj (mapv :name degrees) :P1) occs))))
 
 (defn- chord-inversions 
   "takes a drop and returns its inversions
@@ -189,8 +210,6 @@
                  (if (in? others x) (recur (+ 12 x)) x))]
         (conj acc (vec (sort (conj others oc))))))
     [d]))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn- drops 
   "return a seq of all possible drops of a chord
@@ -281,6 +300,11 @@
           (merge-with-default-options options)
           c-int-map (zipmap (map (f> b> :val)(keys occ-map))
                             (keys occ-map))
+          ; setup the lower interval limit constraint
+          lil-constraint (p lower-limits-ok? (:val root-pitch))
+          ;merge it into options
+          options (assoc options :validator 
+                    (add-validator (:validator options) lil-constraint))
           drops (if invs 
                   (a concat (all-drops occ-map options))
                   (all-drops occ-map options))]
@@ -303,7 +327,7 @@
   ;little hack to don't have to add empty map as last args when we want default options
   (:default [& args]
     (cond 
-      ; if last arg is default-options then we-ve try everything... no possible dispatch
+      ; if args are bartokized and last arg is default-options then we-ve try everything... no possible dispatch
       (and (= (last args) default-options) (= args (b> args))) 
         (pp "no dispatch value for" (b-types args)) 
       ; if args are already bartokized try to add the default-options
@@ -311,94 +335,101 @@
       ; if args are not yet bartokified then try if it yields to a valid dispatch
       :else (a voicings (b> args)))))
 
-(comment 
-  (map (p map :name) (voicings [:Bb-2 :D1] :Mix 8))
-  (map (p map :name) (voicings [:Bb-2 :D1] :Mix 8 {:validator (constantly true)}))
-  (map (p map :name) (voicings [:Bb-2 :Eb1] [:P4 :M6 :M2 :m7 :P5] 8))
-  (map (p map :name) (voicings [:Bb-2 :Eb1] [:P4 :M6 :M2 :m7 :P5] 8 {:max-step 9}))
-  (map (p map :name) (voicings [:Bb-2 :D0] {:P1 1 :M3 1 :P4 1 :M6 1 :m7 1}))
-  (map (p map :name) (voicings [:Bb-2 :Eb0] {:P1 1 :M2 1 :M3 1 :P4 1 :M6 1 :m7 1} {:validator (constantly true)}))
-  (map (p map :name) (voicings :Bb-2 {:P1 1 :M3 1 :P4 1 :M6 1 :m7 1}))
-  (map (p map :name) (voicings :Bb-2 {:P1 1 :M2 1 :M3 1 :P4 1 :M6 1 :m7 1} {:validator (constantly true)})))
-
 ;;;;;;;;;;;;;; examples ;;;;;;;;;;;;;;;
 
-(comment 
-  (time (count (drops [0 2 3 9 11 14 15 21 23] 11 48 (drop-validator b9-free? no-m2-on-top?))))
-  (all-distinct? (drops [0 2 3 9 11 14 15 21 23] 11 48)))
+  (comment 
+    (map (p map :name) (voicings [:Bb-2 :D1] :Mix 8))
+    (map (p map :name) (voicings [:Bb-2 :D1] :Mix 8 {:validator (constantly true)}))
+    (map (p map :name) (voicings [:Bb-2 :Eb1] [:P4 :M6 :M2 :m7 :P5] 8))
+    (map (p map :name) (voicings [:Bb-2 :Eb1] [:P4 :M6 :M2 :m7 :P5] 8 {:max-step 9}))
+    (map (p map :name) (voicings [:Bb-2 :D0]  {:P1 1 :M3 1 :P4 1 :M6 1 :m7 1}))
+    (map (p map :name) (voicings [:Bb-2 :Eb0] {:P1 1 :M2 1 :M3 1 :P4 1 :M6 1 :m7 1} {:validator (constantly true)}))
+    (map (p map :name) (voicings :Bb-2 {:P1 1 :M3 1 :P4 1 :M6 1 :m7 1}))
+    (map (p map :name) (voicings :Bb-2 {:P1 1 :M2 1 :M3 1 :P4 1 :M6 1 :m7 1} {:validator (constantly true)})))
 
-(use 'bartok.midi.midi)
+  (comment 
+    (time (count (drops [0 2 3 9 11 14 15 21 23] 11 48 (drop-validator b9-free? no-m2-on-top?))))
+    (all-distinct? (drops [0 2 3 9 11 14 15 21 23] 11 48)))
 
-(comment 
-  (play-pitch-line 
-    (->> (all-drops {:P1 2 :M6 2 :+4 2 :M3 2 :M7 2} 
-                    {:max-step 7 
-                     :max-size 48
-                     :inversions true})
-         rand-nth
-         rand-nth
-         (map #(pitch (+ 36 %))))))
+  (use 'bartok.midi.midi)
 
-(comment 
-  (play-pitch-line 
-    (->> (all-drops {:P1 2 :+5 2 :+4 2 :M3 2 :M7 2}
-                    {:validator (default-validator-and 
-                                  (grav-center > 0.5))})
-         rand-nth
-         (map #(pitch (+ 44 %))))))
+  (comment 
+    (play-pitch-line 
+      (->> (all-drops {:P1 2 :M6 2 :+4 2 :M3 2 :M7 2} 
+                      {:max-step 7 
+                       :max-size 48
+                       :inversions true})
+           rand-nth
+           rand-nth
+           (map #(pitch (+ 36 %))))))
 
-(comment 
-  (play-chord
-    (rand-nth 
-      (voicings 
-        [:C-1 :D#1]
-        {:P1 1 :M6 1 :+4 1 :M3 1 :#2 1 :M7 1} 
-        {:validator (default-validator-and
-                      (grav-center > 0.5))}))))
+  (comment 
+    (play-pitch-line 
+      (->> (all-drops {:P1 2 :+5 2 :+4 2 :M3 2 :M7 2}
+                      {:validator (default-validator-and 
+                                    (grav-center > 0.5))})
+           rand-nth
+           (map #(pitch (+ 44 %))))))
 
-(comment 
-  (play-chord
-    (rand-nth 
-      (voicings 
-        :Ab-2 
-        {:P1 1 :P5 1 :P4 1 :M6 1 :m7 1 :M2 1 :M3 1} 
-        {:max-size 48
-         :validator (default-validator-and
-                      (grav-center > 0.6))}))))
+  (comment 
+    (play-chord
+      (rand-nth 
+        (voicings 
+          [:C-1 :D#1]
+          {:P1 1 :M6 1 :+4 1 :M3 1 :#2 1 :M7 1} 
+          {:validator (default-validator-and
+                        (grav-center > 0.5))}))))
 
-(comment 
-  (play-chord
-    (rand-nth 
-      (voicings 
-        :C-1 
-        [:M3 :M2 :P5 :M7] 6))))
+  (comment 
+    (play-chord
+      (rand-nth 
+        (voicings 
+          :Ab-2 
+          {:P1 1 :P5 1 :P4 1 :M6 1 :m7 1 :M2 1 :M3 1} 
+          {:max-size 48
+           :validator (default-validator-and
+                        (grav-center > 0.6))}))))
 
-(comment 
-  (play-chord
-    (rand-nth 
-      (voicings 
-        :Bb-2  
-        [:M6 :P4 :M2 :P5 :m7] 6))))
+  (comment 
+    (play-chord
+      (rand-nth 
+        (voicings 
+          :C-1 
+          [:M3 :M2 :P5 :M7] 6))))
 
-(comment 
-  (play-chord
-    (rand-nth 
-      (voicings 
-        :Eb-1  
-        [:M3 :M2 :P5 :M7] 6))))
+  (comment 
+    (play-chord
+      (rand-nth 
+        (voicings 
+          :Bb-2  
+          [:M6 :P4 :M2 :P5 :m7] 6))))
 
-(comment 
-  (play-chord
-    (rand-nth 
-      (voicings 
-        :Ab-2  
-        [:M6 :P4 :M2 :P5 :m7] 6))))
+  (comment 
+    (play-chord
+      (rand-nth 
+        (voicings 
+          :Eb-1  
+          [:M3 :M2 :P5 :M7] 6))))
 
-(comment 
-  (play-chord
-    (rand-nth 
-      (voicings 
-        :Db-1  
-        [:M3 :M2 :P5 :M7] 6))))
-; (uniformity [1 2 3 4 50])
+  (comment 
+    (play-chord
+      (rand-nth 
+        (voicings 
+          :Ab-2  
+          [:M6 :P4 :M2 :P5 :m7] 6
+          {:validator (default-validator-and 
+                        (amplitude or= [29 33]) 
+                        (median > 12))}))))
+
+  (comment 
+    (play-chord
+      (rand-nth 
+        (voicings 
+          :Db-2  
+          [:M3 :+4 :M6 :P5 :M7] 6))))
+
+
+
+
+
 
